325eeaa612219cf74c17c068a53eb8eb6b516c44
tlp19 <tlp19@ic.ac.uk>
2023-10-12 17:01:55 +0100

    Added comments for Serial decoding

--- WS2-Arduino/src/main.cpp
+++ WS2-Arduino/src/main.cpp
@@ -33,22 +33,30 @@ void loop() {
 
     // Decode message received from Serial
     while(Serial.available()){
+        // Read one character at a time
         char received = (char)Serial.read();
-        
+        // Check what the character is, and decide what to do accordingly
         if(received=='s'){
+            // If the character is 's', we will be reading the slider ID next
             readingSliderID = true;
             readingSliderValue = false;
 
         } else if(received=='v'){
+            // If the character is 'v', we will be reading the slider value next
             readingSliderID = false;
             readingSliderValue = true;
 
         } else if(received=='e'){
-            // Clean-up the received strings
+            // If the character is 'e', we have finished reading everything
+
+            // Clean-up the received strings/texts (good practice, just in case)
             receivedSliderID.trim();
             receivedSliderValue.trim();
-            // Update the stored slider values
+
+            // Update the relevant stored slider value to be the new received value
             if(receivedSliderID=="1"){
+                // If the slider ID is 1, we will update the slider 1 value
+                // We will also have to convert the received string/text to a floating point number (= decimal number)
                 slider1Value = receivedSliderValue.toFloat();
             } else if(receivedSliderID=="2"){
                 slider2Value = receivedSliderValue.toFloat();
@@ -57,17 +65,21 @@ void loop() {
             } else if(receivedSliderID=="4"){
                 slider4Value = receivedSliderValue.toFloat();
             }
-            // Reset the flags and received strings
+
+            // Reset the flags and received strings/texts
             readingSliderID = false;
             readingSliderValue = false;
             receivedSliderID = "";
             receivedSliderValue = "";
-            //break;
 
         } else if(readingSliderID){
+            // If the character is not a letter, it should be a number or a decimal point '.'
+            // So if we are currently reading the slider ID, we will add the number/dot to the current received ID string/text
             receivedSliderID += received;
 
         } else if(readingSliderValue){
+            // If the character is not a letter, it should be a number or a decimal point '.'
+            // So if we are currently reading the slider value, we will add the number/dot to the current received value string/text
             receivedSliderValue += received;
         }
     }
